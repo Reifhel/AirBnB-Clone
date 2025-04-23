5d38a2cf849b84220454811eddd0463d573de0eb
Reifhel <rafaelherdt1@gmail.com>
2025-04-23 18:14:32 -0300

    (main) Adding the node backend and start of the express API

---
+++ back-end/index.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import express from "express";
+import UserRoutes from "./domains/users/routes.js";
+
+const app = express();
+const { PORT } = process.env;
+
+// Only getting the json request
+app.use(express.json());
+app.use("/users", UserRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor on na porta ${PORT}`);
+});
